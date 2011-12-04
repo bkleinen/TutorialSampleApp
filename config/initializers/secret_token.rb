264b70a93b98aebd281548aeea4e9d2540fa69f1
B Kleinen <kleinen@infrastructure.de>
2011-12-04 22:07:13 +0100

    created tutorial 3.1.3 & Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutorial31::Application.config.secret_token = '75c6a219152bef251a1b9713bb3bec822de5b986751f732fec0704e46f908129246a772f37e9a4d66f55090f854f932365a16302d5e790b40fa8b564e5325a24'
